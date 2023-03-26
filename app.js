3cffcec5aa8bdcdf94309af449b3bae8ca85af0a
ThyDrSlen <drslen9@gmail.com>
2023-03-26 00:08:34 -0400

    start

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    let movieData = [];
 
-    //on load fetch movie data
-    fetch("https://api.themoviedb.org/3/movie/2?api_key=1025d8bf81d519a9ad57e875dc63ff51&language=en-US")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-       
-    })
-
-
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ea9884afb2msh82587699b006427p15b16ejsn74808ecc0952',
+            'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://moviesdatabase.p.rapidapi.com/titles/tt0903747/', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
     
 
 })
