2324ff5d6f393217c7140d9b2bb0e15af944c84b
ThyDrSlen <drslen9@gmail.com>
2023-03-27 12:37:16 -0400

    more done

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+import { API_KEY } from './api-key.js';
+
 document.addEventListener("DOMContentLoaded", () => {
+  let arrOfStartPosters = document.querySelectorAll(".start-poster");
 
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': API_KEY,
+      'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    }
+  };
   
+  let randomPage = Math.floor(Math.random() * Math.floor(5) + 1);
+  async function fetchThenSetStartPosters() {
 
+    try {
+
+      const response = await fetch(`https://moviesdatabase.p.rapidapi.com/titles?list=most_pop_movies&limit=50&page=${randomPage}`, options)
+      const data = await response.json();
+      console.log(data)
+      arrOfStartPosters.forEach(poster => poster.src = data.results[Math.floor(Math.random() * Math.floor(50))].primaryImage.url)
+
+
+    }
+    catch (error) {
+      console.error('Error fetching data:', error);
+      fetchThenSetStartPosters()
+    }
+  }
+
+  fetchThenSetStartPosters();
+
+  //   function setStartPosters() {
+
+  //   }
+  //   setStartPosters();
 });
